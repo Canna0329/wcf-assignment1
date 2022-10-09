30bc26c9a7b26372567c3f043a94799ba73f9d24
Canna0329 <47868758+Canna0329@users.noreply.github.com>
2022-10-09 15:27:28 -0400

    Created WCF-Assignment1

---
+++ WCFConsoleAssignment1/WCFConsoleAssignment1/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Threading.Tasks;
+using ServiceReference1;
+
+namespace WCFConsoleAssignment1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("1.Prime number\n" +
+                    "2.Sum of digits\n" +
+                    "3.Reverse a string\n" +
+                    "4.Print HTML tags\n" +
+                    "5.Sort 5 numbers\n" +
+                    "6.Exit");
+                string selection = Console.ReadLine();
+                if (selection.Equals("6")) break;
+                else await Menu(selection);
+            }
+        }
+
+        private static async Task Menu(string selection)
+        {
+            Service1Client client = new Service1Client();
+            string input = "";
+            int result = 0;
+            switch (selection)
+            {
+                case "1":
+                    {
+                        Console.WriteLine("Enter integer to check for Prime");
+                        input = Console.ReadLine();
+                        if (int.TryParse(input, out result))
+                        {
+                            IsPrimeRequest isPrime = new IsPrimeRequest(result);
+                            var value = await client.IsPrimeAsync(isPrime);
+                            IsPrimeResponse primeResponse = value;
+                            Console.WriteLine(primeResponse.IsPrimeResult);
+                        }
+                        else Console.WriteLine("Not an int");
+                    }
+                    break;
+                case "2":
+                    {
+                        Console.WriteLine("Enter string to sum up all included digits");
+                        input = Console.ReadLine();
+                        DigitSumRequest digitSum = new DigitSumRequest(input);
+                        var value = await client.DigitSumAsync(digitSum);
+                        DigitSumResponse digitResponse = value;
+                        Console.WriteLine(digitResponse.DigitSumResult);
+                    }
+                    break;
+                case "3":
+                    {
+                        Console.WriteLine("Enter String to reverse");
+                        input = Console.ReadLine();
+                        ReverseStringRequest stringRequest = new ReverseStringRequest(input);
+                        var value = await client.ReverseStringAsync(stringRequest);
+                        ReverseStringResponse stringResponse = value;
+                        Console.WriteLine(stringResponse.ReverseStringResult);
+                    }
+                    break;
+                case "4":
+                    { 
+                        Console.WriteLine("Enter HTML tag");
+                        input = Console.ReadLine();
+                        Console.WriteLine("Enter HTML tag contents");
+                        string content = Console.ReadLine();
+                        HTMLBuildRequest buildRequest = new HTMLBuildRequest(input, content);
+                        var value = await client.HTMLBuildAsync(buildRequest);
+                        HTMLBuildResponse buildResponse = value;
+                        Console.WriteLine(buildResponse.HTMLBuildResult);
+                    }
+                    break;
+                case "5":
+                    { 
+                        Console.WriteLine("Enter Sort Style");
+                        input = Console.ReadLine();
+                        Console.WriteLine("Enter 5 Numbers to sort");
+                        int[] content = new int[5];
+                        for (int i = 0; i < content.Length; i++)
+                        {
+                            if (int.TryParse(Console.ReadLine(),out result))
+                            {
+                            content[i] = result;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid Integer try again");
+                                i--;
+                            }                            
+                        }
+                        SortNumbersRequest numbersRequest = new SortNumbersRequest(input, content);
+                        var value = await client.SortNumbersAsync(numbersRequest);
+                        SortNumbersResponse numbersResponse = value;
+                        Console.WriteLine(numbersResponse.SortNumbersResult);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ WcfService1/WcfService1/Service1.svc.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string DigitSum(string value)
+        {
+            int total = 0;
+            int current = 0;
+            foreach (char c in value)
+            {
+                if (int.TryParse(c.ToString(), out current))
+                {
+                    total += current;
+                }
+            }
+            return total.ToString();
+        }
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public string HTMLBuild(string tag, string value)
+        {
+            string output = "";
+
+            output += $"<{tag}>{value}</{tag}>";
+
+            return output;
+        }
+
+        public string IsPrime(int value)
+        {
+            string message = "prime number";
+
+            for (int i = 2; i <= value/2; i++)
+            {
+                if (value % i == 0)
+                {
+                    message = "not a " + message;
+                    break;
+                }
+            }
+            if (value<=1)
+            {
+                message = "not a " + message;
+            }
+
+            return message;
+        }
+
+        public string ReverseString(string value)
+        {
+            string output = "";
+            foreach (char c in value)
+            {
+                output = c + output;
+            }
+            return output;
+        }
+
+        public string SortNumbers(string type, int[] values)
+        {
+            string output = "";
+            Array.Sort(values);
+            if (type.Equals("Ascending"))
+            {
+                for (int i = 0; i < values.Length; i++)
+                {
+                    output += values[i].ToString()+",";
+                }
+                output = output.Substring(0, output.Length - 1);
+            }
+            else if (type.Equals("Descending"))
+            {
+                for (int i = values.Length - 1; i >= 0; i--)
+                {
+                    output += values[i].ToString() + ",";
+                }
+                output = output.Substring(0, output.Length - 1);
+            }
+            else output = "No Valid Sort";
+
+            return output;
+        }
+    }
+}
